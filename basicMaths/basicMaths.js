3ec406e051acc399aae6d9880118f6300c2eb639
prem315 <prem.patel3105@gmail.com>
2023-10-05 10:29:21 +0530

    palindrome

--- basicMaths/basicMaths.js
+++ basicMaths/basicMaths.js
@@ -8,4 +8,20 @@ function countDigits(num) {
   return count;
 }
 
-export { countDigits };
+// check palindrome
+function checkPalindrome(num) {
+  let number = num;
+  let reverse = 0;
+  while (num > 0) {
+    let lastDigit = num % 10;
+    num = Math.floor(num / 10);
+    reverse = 10 * reverse + lastDigit;
+  }
+  if (number === reverse) {
+    return "palindrome";
+  } else {
+    return "not palindrome";
+  }
+}
+
+export { countDigits, checkPalindrome };
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-import { countDigits } from "./basicMaths/basicMaths.js";
+import { countDigits, checkPalindrome } from "./basicMaths/basicMaths.js";
 
 console.log("running....");
 
 const res = countDigits(123);
+
+const res2 = checkPalindrome(121);
 console.log(res);
+console.log(res2);
